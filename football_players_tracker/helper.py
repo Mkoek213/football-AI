4df1a792c648be445cc9873b3d86b7436adcb4c7
Mkoek213 <mikolaj.kolek2@gmail.com>
2024-09-21 20:13:45 +0200

    added imports

--- football_players_tracker/helper.py
+++ football_players_tracker/helper.py
@@ -1,4 +1,7 @@
+from dataclasses import dataclass, field
+import cv2
 import torch
+import numpy.typing as npt
 import numpy as np
 import supervision as sv
 from tqdm import tqdm
@@ -7,6 +10,7 @@ import umap.umap_ as umap
 from sklearn.cluster import KMeans
 from typing import Generator, Iterable, List, TypeVar
 from ultralytics import YOLO
+from typing import List, Tuple, Optional
 
 V = TypeVar("V")
 def extract_crops(model, source_video_path: str, stride: int = 30) -> List[np.ndarray]:
@@ -194,3 +198,339 @@ class TeamClassifier:
 
         # Predict team labels using the trained clustering model
         return self.cluster_model.predict(projections)
+
+
+@dataclass
+class SoccerPitchConfiguration:
+    width: int = 7000  # [cm]
+    length: int = 12000  # [cm]
+    penalty_box_width: int = 4100  # [cm]
+    penalty_box_length: int = 2015  # [cm]
+    goal_box_width: int = 1832  # [cm]
+    goal_box_length: int = 550  # [cm]
+    centre_circle_radius: int = 915  # [cm]
+    penalty_spot_distance: int = 1100  # [cm]
+
+    @property
+    def vertices(self) -> List[Tuple[int, int]]:
+        return [
+            (0, 0),  # 1
+            (0, (self.width - self.penalty_box_width) / 2),  # 2
+            (0, (self.width - self.goal_box_width) / 2),  # 3
+            (0, (self.width + self.goal_box_width) / 2),  # 4
+            (0, (self.width + self.penalty_box_width) / 2),  # 5
+            (0, self.width),  # 6
+            (self.goal_box_length, (self.width - self.goal_box_width) / 2),  # 7
+            (self.goal_box_length, (self.width + self.goal_box_width) / 2),  # 8
+            (self.penalty_spot_distance, self.width / 2),  # 9
+            (self.penalty_box_length, (self.width - self.penalty_box_width) / 2),  # 10
+            (self.penalty_box_length, (self.width - self.goal_box_width) / 2),  # 11
+            (self.penalty_box_length, (self.width + self.goal_box_width) / 2),  # 12
+            (self.penalty_box_length, (self.width + self.penalty_box_width) / 2),  # 13
+            (self.length / 2, 0),  # 14
+            (self.length / 2, self.width / 2 - self.centre_circle_radius),  # 15
+            (self.length / 2, self.width / 2 + self.centre_circle_radius),  # 16
+            (self.length / 2, self.width),  # 17
+            (
+                self.length - self.penalty_box_length,
+                (self.width - self.penalty_box_width) / 2
+            ),  # 18
+            (
+                self.length - self.penalty_box_length,
+                (self.width - self.goal_box_width) / 2
+            ),  # 19
+            (
+                self.length - self.penalty_box_length,
+                (self.width + self.goal_box_width) / 2
+            ),  # 20
+            (
+                self.length - self.penalty_box_length,
+                (self.width + self.penalty_box_width) / 2
+            ),  # 21
+            (self.length - self.penalty_spot_distance, self.width / 2),  # 22
+            (
+                self.length - self.goal_box_length,
+                (self.width - self.goal_box_width) / 2
+            ),  # 23
+            (
+                self.length - self.goal_box_length,
+                (self.width + self.goal_box_width) / 2
+            ),  # 24
+            (self.length, 0),  # 25
+            (self.length, (self.width - self.penalty_box_width) / 2),  # 26
+            (self.length, (self.width - self.goal_box_width) / 2),  # 27
+            (self.length, (self.width + self.goal_box_width) / 2),  # 28
+            (self.length, (self.width + self.penalty_box_width) / 2),  # 29
+            (self.length, self.width),  # 30
+            (self.length / 2 - self.centre_circle_radius, self.width / 2),  # 31
+            (self.length / 2 + self.centre_circle_radius, self.width / 2),  # 32
+        ]
+
+    edges: List[Tuple[int, int]] = field(default_factory=lambda: [
+        (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (7, 8),
+        (10, 11), (11, 12), (12, 13), (14, 15), (15, 16),
+        (16, 17), (18, 19), (19, 20), (20, 21), (23, 24),
+        (25, 26), (26, 27), (27, 28), (28, 29), (29, 30),
+        (1, 14), (2, 10), (3, 7), (4, 8), (5, 13), (6, 17),
+        (14, 25), (18, 26), (23, 27), (24, 28), (21, 29), (17, 30)
+    ])
+
+    labels: List[str] = field(default_factory=lambda: [
+        "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+        "11", "12", "13", "15", "16", "17", "18", "20", "21", "22",
+        "23", "24", "25", "26", "27", "28", "29", "30", "31", "32",
+        "14", "19"
+    ])
+
+    colors: List[str] = field(default_factory=lambda: [
+        "#FF1493", "#FF1493", "#FF1493", "#FF1493", "#FF1493", "#FF1493",
+        "#FF1493", "#FF1493", "#FF1493", "#FF1493", "#FF1493", "#FF1493",
+        "#FF1493", "#00BFFF", "#00BFFF", "#00BFFF", "#00BFFF", "#FF6347",
+        "#FF6347", "#FF6347", "#FF6347", "#FF6347", "#FF6347", "#FF6347",
+        "#FF6347", "#FF6347", "#FF6347", "#FF6347", "#FF6347", "#FF6347",
+        "#00BFFF", "#00BFFF"
+    ])
+
+
+def draw_pitch(
+    config: SoccerPitchConfiguration,
+    background_color: sv.Color = sv.Color(34, 139, 34),
+    line_color: sv.Color = sv.Color.WHITE,
+    padding: int = 50,
+    line_thickness: int = 4,
+    point_radius: int = 8,
+    scale: float = 0.1
+) -> np.ndarray:
+    """
+    Draws a soccer pitch with specified dimensions, colors, and scale.
+
+    Args:
+        config (SoccerPitchConfiguration): Configuration object containing the
+            dimensions and layout of the pitch.
+        background_color (sv.Color, optional): Color of the pitch background.
+            Defaults to sv.Color(34, 139, 34).
+        line_color (sv.Color, optional): Color of the pitch lines.
+            Defaults to sv.Color.WHITE.
+        padding (int, optional): Padding around the pitch in pixels.
+            Defaults to 50.
+        line_thickness (int, optional): Thickness of the pitch lines in pixels.
+            Defaults to 4.
+        point_radius (int, optional): Radius of the penalty spot points in pixels.
+            Defaults to 8.
+        scale (float, optional): Scaling factor for the pitch dimensions.
+            Defaults to 0.1.
+
+    Returns:
+        np.ndarray: Image of the soccer pitch.
+    """
+    scaled_width = int(config.width * scale)
+    scaled_length = int(config.length * scale)
+    scaled_circle_radius = int(config.centre_circle_radius * scale)
+    scaled_penalty_spot_distance = int(config.penalty_spot_distance * scale)
+
+    pitch_image = np.ones(
+        (scaled_width + 2 * padding,
+         scaled_length + 2 * padding, 3),
+        dtype=np.uint8
+    ) * np.array(background_color.as_bgr(), dtype=np.uint8)
+
+    for start, end in config.edges:
+        point1 = (int(config.vertices[start - 1][0] * scale) + padding,
+                  int(config.vertices[start - 1][1] * scale) + padding)
+        point2 = (int(config.vertices[end - 1][0] * scale) + padding,
+                  int(config.vertices[end - 1][1] * scale) + padding)
+        cv2.line(
+            img=pitch_image,
+            pt1=point1,
+            pt2=point2,
+            color=line_color.as_bgr(),
+            thickness=line_thickness
+        )
+
+    centre_circle_center = (
+        scaled_length // 2 + padding,
+        scaled_width // 2 + padding
+    )
+    cv2.circle(
+        img=pitch_image,
+        center=centre_circle_center,
+        radius=scaled_circle_radius,
+        color=line_color.as_bgr(),
+        thickness=line_thickness
+    )
+
+    penalty_spots = [
+        (
+            scaled_penalty_spot_distance + padding,
+            scaled_width // 2 + padding
+        ),
+        (
+            scaled_length - scaled_penalty_spot_distance + padding,
+            scaled_width // 2 + padding
+        )
+    ]
+    for spot in penalty_spots:
+        cv2.circle(
+            img=pitch_image,
+            center=spot,
+            radius=point_radius,
+            color=line_color.as_bgr(),
+            thickness=-1
+        )
+
+    return pitch_image
+
+
+class ViewTransformer:
+    def __init__(
+            self,
+            source: npt.NDArray[np.float32],
+            target: npt.NDArray[np.float32]
+    ) -> None:
+
+        source = source.astype(np.float32)
+        target = target.astype(np.float32)
+        self.m, _ = cv2.findHomography(source, target)
+
+    def transform_points(
+            self,
+            points: npt.NDArray[np.float32]
+    ) -> npt.NDArray[np.float32]:
+        if len(points) > 0:
+            points = points.reshape(-1, 1, 2).astype(np.float32)
+            points = cv2.perspectiveTransform(points, self.m)
+            return points.reshape(-1, 2).astype(np.float32)
+        else:
+            return []
+
+
+def draw_paths_on_pitch(
+    config: SoccerPitchConfiguration,
+    paths: List[np.ndarray],
+    color: sv.Color = sv.Color.WHITE,
+    thickness: int = 2,
+    padding: int = 50,
+    scale: float = 0.1,
+    pitch: Optional[np.ndarray] = None
+) -> np.ndarray:
+    """
+    Draws paths on a soccer pitch.
+
+    Args:
+        config (SoccerPitchConfiguration): Configuration object containing the
+            dimensions and layout of the pitch.
+        paths (List[np.ndarray]): List of paths, where each path is an array of (x, y)
+            coordinates.
+        color (sv.Color, optional): Color of the paths.
+            Defaults to sv.Color.WHITE.
+        thickness (int, optional): Thickness of the paths in pixels.
+            Defaults to 2.
+        padding (int, optional): Padding around the pitch in pixels.
+            Defaults to 50.
+        scale (float, optional): Scaling factor for the pitch dimensions.
+            Defaults to 0.1.
+        pitch (Optional[np.ndarray], optional): Existing pitch image to draw paths on.
+            If None, a new pitch will be created. Defaults to None.
+
+    Returns:
+        np.ndarray: Image of the soccer pitch with paths drawn on it.
+    """
+    if pitch is None:
+        pitch = draw_pitch(
+            config=config,
+            padding=padding,
+            scale=scale
+        )
+
+    for path in paths:
+        scaled_path = [
+            (
+                int(point[0] * scale) + padding,
+                int(point[1] * scale) + padding
+            )
+            for point in path if point.size > 0
+        ]
+
+        if len(scaled_path) < 2:
+            continue
+
+        for i in range(len(scaled_path) - 1):
+            cv2.line(
+                img=pitch,
+                pt1=scaled_path[i],
+                pt2=scaled_path[i + 1],
+                color=color.as_bgr(),
+                thickness=thickness
+            )
+
+        return pitch
+
+
+def draw_points_on_pitch(
+    config: SoccerPitchConfiguration,
+    xy: np.ndarray,
+    face_color: sv.Color = sv.Color.RED,
+    edge_color: sv.Color = sv.Color.BLACK,
+    radius: int = 10,
+    thickness: int = 2,
+    padding: int = 50,
+    scale: float = 0.1,
+    pitch: Optional[np.ndarray] = None
+) -> np.ndarray:
+    """
+    Draws points on a soccer pitch.
+
+    Args:
+        config (SoccerPitchConfiguration): Configuration object containing the
+            dimensions and layout of the pitch.
+        xy (np.ndarray): Array of points to be drawn, with each point represented by
+            its (x, y) coordinates.
+        face_color (sv.Color, optional): Color of the point faces.
+            Defaults to sv.Color.RED.
+        edge_color (sv.Color, optional): Color of the point edges.
+            Defaults to sv.Color.BLACK.
+        radius (int, optional): Radius of the points in pixels.
+            Defaults to 10.
+        thickness (int, optional): Thickness of the point edges in pixels.
+            Defaults to 2.
+        padding (int, optional): Padding around the pitch in pixels.
+            Defaults to 50.
+        scale (float, optional): Scaling factor for the pitch dimensions.
+            Defaults to 0.1.
+        pitch (Optional[np.ndarray], optional): Existing pitch image to draw points on.
+            If None, a new pitch will be created. Defaults to None.
+
+    Returns:
+        np.ndarray: Image of the soccer pitch with points drawn on it.
+    """
+    if pitch is None:
+        pitch = draw_pitch(
+            config=config,
+            padding=padding,
+            scale=scale
+        )
+
+    for point in xy:
+        scaled_point = (
+            int(point[0] * scale) + padding,
+            int(point[1] * scale) + padding
+        )
+        cv2.circle(
+            img=pitch,
+            center=scaled_point,
+            radius=radius,
+            color=face_color.as_bgr(),
+            thickness=-1
+        )
+        cv2.circle(
+            img=pitch,
+            center=scaled_point,
+            radius=radius,
+            color=edge_color.as_bgr(),
+            thickness=thickness
+        )
+
+    return pitch
+
+
+
